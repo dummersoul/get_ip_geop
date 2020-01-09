c2f510ea900787d5551c6b8c87779a19758a29e8
dummersoul <dummersoul@hotmail.com>
2020-01-09 15:03:59 +0800

    更新一些细节

--- geo.py
+++ geo.py
@@ -25,10 +25,8 @@ def get_ip():  # 获取本地网络ip
 
 def crawl_geo(ip):
     ip_geo_url = "https://api.ipplus360.com/ip/geo/v1/city/?key=%s&ip=%s&coordsys=WGS84" % (key, ip)
-    print(ip_geo_url)
     page = requests.get(ip_geo_url).content.decode('utf-8')  # 网页text
     page = json.loads(page)  # 将str格式转换为dict格式
-    # print(page)
     global country, prov, city, longitude, latitude
     if page['code'] == 'Success':
         country = page['data']['country']  # 国家
@@ -36,7 +34,9 @@ def crawl_geo(ip):
         city = page['data']['multiAreas'][0]['city']  # 城市
         longitude = page['data']['multiAreas'][0]['lng']  # 经度
         latitude = page['data']['multiAreas'][0]['lat']  # 纬度
-
+    else:
+        print("查询失败，请检查所查询的ip是否输入正确")
+        return
     return {'ip': ip, 'country': country, 'prov': prov, 'city': city,
             'locate': '经度:%s 纬度:%s' % (longitude, latitude)}
 
@@ -44,3 +44,4 @@ def crawl_geo(ip):
 if __name__ == '__main__':
     get_ip()
     print(crawl_geo('36.5.132.158'))  # 字典类型的地理信息
+    print(crawl_geo('45.33.14.28'))
