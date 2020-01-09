b7de52d27f8c8e51dff9ba4a150a03f6d820cadc
dummersoul <dummersoul@hotmail.com>
2020-01-09 15:05:31 +0800

    去除一些用不到的模块

--- geo.py
+++ geo.py
@@ -1,12 +1,10 @@
 # -*- coding: UTF-8 -*-
 import json
 import requests
-from urllib.request import urlopen
 from lxml import etree
 
 key = '21c8ET627t6DXLhKX2666Wt5BS1oyySW9GAWhmspxUM5Pd9mYDbJk4zL0y4M0GYn'
 global country, prov, city, longitude, latitude
-
 
 # page = {'code': 'Success',
 #         'data': {'continent': '亚洲', 'country': '中国', 'owner': '中国电信',
@@ -16,16 +14,15 @@ global country, prov, city, longitude, latitude
 
 
 def get_ip():  # 获取本地网络ip
-    page = requests.get("https://ip.cn/")
-    html_text = etree.HTML(page.text)
-    ip = html_text.xpath('//span/text()')[5].replace(':', '').replace(' ', '')
-    print(ip)
+    page = requests.get("https://ip.cn/")   # 获取网页
+    html_text = etree.HTML(page.text)   # 将html转化为element元素
+    ip = html_text.xpath('//span/text()')[5].replace(':', '').replace(' ', '')  # 用xpath在element元素中获取所需要的信息
     return ip
 
 
 def crawl_geo(ip):
-    ip_geo_url = "https://api.ipplus360.com/ip/geo/v1/city/?key=%s&ip=%s&coordsys=WGS84" % (key, ip)
-    page = requests.get(ip_geo_url).content.decode('utf-8')  # 网页text
+    ip_geo_url = "https://api.ipplus360.com/ip/geo/v1/city/?key=%s&ip=%s&coordsys=WGS84" % (key, ip)    # 关键的API网址，其中的key是买来的（免费试用30天）
+    page = requests.get(ip_geo_url).content.decode('utf-8')  # 网页text，编码格式为中文
     page = json.loads(page)  # 将str格式转换为dict格式
     global country, prov, city, longitude, latitude
     if page['code'] == 'Success':
@@ -42,6 +39,6 @@ def crawl_geo(ip):
 
 
 if __name__ == '__main__':
-    get_ip()
-    print(crawl_geo('36.5.132.158'))  # 字典类型的地理信息
+    print("本机ip上网代理地址为", get_ip())
+    print(crawl_geo('36.5.132.158'))  # 打印出字典类型的地理信息
     print(crawl_geo('45.33.14.28'))
